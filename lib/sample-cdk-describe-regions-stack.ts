65953858d17eb3ec814bb37dd37c53eba9d072b7
yamatatsu <the.kinnikuman@gmail.com>
2022-10-04 19:28:35 +0900

    init

--- lib/sample-cdk-describe-regions-stack.ts
+++ lib/sample-cdk-describe-regions-stack.ts
@@ -1,16 +1,26 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as cr from "aws-cdk-lib/custom-resources";
 
 export class SampleCdkDescribeRegionsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const regions = new cr.AwsCustomResource(this, "DescribeRegions", {
+      onCreate: {
+        service: "EC2",
+        action: "describeRegions",
+        physicalResourceId: cr.PhysicalResourceId.of(
+          "DescribeRegionsPhysicalResourceId"
+        ),
+      },
+      policy: cr.AwsCustomResourcePolicy.fromSdkCalls({
+        resources: cr.AwsCustomResourcePolicy.ANY_RESOURCE,
+      }),
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'SampleCdkDescribeRegionsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new cdk.CfnOutput(this, "eu-north-1_OptInStatus", {
+      value: regions.getResponseField("Regions.0.OptInStatus"),
+    });
   }
 }
