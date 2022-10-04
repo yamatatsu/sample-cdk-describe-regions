36aef21c3dc99766b51552e2298e7619a93bfc94
yamatatsu <the.kinnikuman@gmail.com>
2022-10-04 18:27:12 +0900

    Initial commit

---
+++ test/sample-cdk-describe-regions.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SampleCdkDescribeRegions from '../lib/sample-cdk-describe-regions-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/sample-cdk-describe-regions-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SampleCdkDescribeRegions.SampleCdkDescribeRegionsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
